7fb17f8c82a70d80744573d89faeb0bab8cb5845
wagurano <wagurano@gmail.com>
2026-04-12 20:04:00 +0900

    소개페이지 추가합니다

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "searches#index"
   get "/search", to: "searches#search", as: :search
+  get "/about", to: "pages#about", as: :about
 
   resources :disease_cases, only: :show
 
