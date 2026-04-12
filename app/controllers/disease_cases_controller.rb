f7fd4c901dce3123b7ad1a25feed3d1784f23aa6
wagurano <wagurano@gmail.com>
2026-04-12 21:01:00 +0900

    SearchesController 를 지우고 DiseaseCasesController 에서 검색합니다
    - SearchesController 삭제하고 검색 메서드 통합
    - 검색 뷰 app/views/searches 에서 app/views/disease_cases 으로 이동
    - 루트 경로 변경 root to disease_cases#index
    - /search 경로 유지 (disease_cases#search 처리)

--- app/controllers/disease_cases_controller.rb
+++ app/controllers/disease_cases_controller.rb
@@ -1,5 +1,53 @@
 class DiseaseCasesController < ApplicationController
+  MAX_SEARCH_RESULTS = 500
+
+  def index
+    perform_search
+  end
+
+  def search
+    perform_search
+  end
+
   def show
     @disease_case = DiseaseCase.find_by(case_no: params[:case_no])
+  end
+
+  private
+
+  def perform_search
+    @scope, @fallback = DiseaseCase.search(search_params)
+    @pagy, @cases = paginate(@scope)
+    @metadata = build_metadata
+
+    log_search_event
+  end
+
+  def paginate(scope)
+    pagy(scope, items: 12, max_items: MAX_SEARCH_RESULTS)
+  end
+
+  def build_metadata
+    over_cap = @pagy.count >= MAX_SEARCH_RESULTS
+
+    {
+      total_count: over_cap ? nil : @pagy.count,
+      over_cap: over_cap,
+      used_fallback: @fallback
+    }
+  end
+
+  def search_params
+    params.permit(:q, :result, :year, :disease_category, :body_part, :decided_on_from, :decided_on_to, :sort, :commit)
+  end
+
+  def log_search_event
+    Rails.logger.info({
+      event: "search",
+      query: params[:q],
+      result_count: @metadata[:total_count],
+      over_cap: @metadata[:over_cap],
+      used_fallback: @fallback
+    }.to_json)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root "searches#index"
-  get "/search", to: "searches#search", as: :search
+  root "disease_cases#index"
+  get "/search", to: "disease_cases#search", as: :search
   get "/about", to: "pages#about", as: :about
 
-  resources :disease_cases, param: :case_no, only: :show
+  resources :disease_cases, param: :case_no, only: [ :index, :show ] do
+    collection do
+      get :search
+    end
+  end
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 end
